4db6acdc08275eeb6ae2ea21d154309cd76fb1c1
Terrific789 <izunnaegba2@gmail.com>
2025-03-23 11:14:45 +0100

    week02

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,24 @@ document.getElementById("currentyear").innerHTML = `&copy; ${currentYear} | `;
 // Get the last modified date of the document
 const lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+
+// function showNavbar() {
+//     var menu = document.getElementById("mobile");
+//     if (menu.style.display === "flex") {
+//         menu.style.display = "none";
+//     } else {
+//         menu.style.display = "flex";
+//     }
+// }
+
+
+
+function showNavbar() {
+    const sidebar = document.querySelector('#mobile');
+    sidebar.style.display ="block";
+}
+
+function hideNavbar() {
+    const hidebar = document.querySelector('#mobile');
+    hidebar.style.display ="none";
+}
